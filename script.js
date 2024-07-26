2b2f5e5d8c72dab4ef1f28c8b32872a7c22f69dd
Imre <rabl.imre95@gmail.com>
2024-07-26 19:49:24 +0200

    finished friday dom manipulation

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 const characterCard = ({name, species, status, image}) => {
-    // console.log(characterData);
-    // const name = characterData.name;
-    // const species = charaterData.species;
-    // const status = characterData.status;
-    // const image = charactetrData.image;
-
-    // const {name, species, status, image} = characterData;
 
    return  `<div class="card"> 
    <h2>${name}</h2>
    <h3 class ="species">${species}</h3>
-   <h3>kismacska</h3>
    <h4>${status}</h4>
    <img src=${image}>
    </div>`;
@@ -21,22 +13,83 @@ const charactersComponent = (charactersData) => `
     ${charactersData.map(characterData => characterCard(characterData)).join("")}
     </div)
 `;
+
+const nextButtonComponent = (type) => `
+<button class=${type}>${type}</button>
+`
+
+
 const makeDomFromData = (data, rootElement) => {
-    rootElement.insertAdjacentHTML("beforeend",charactersComponent(data.results))
+    rootElement.innerHTML = "";
+    console.log(data)
+
+
+ if(data.info.next) rootElement.insertAdjacentHTML("beforeend", nextButtonComponent("next"))
+ if(data.info.prev) rootElement.insertAdjacentHTML("beforeend", nextButtonComponent("prev"))
+
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach(button => button.addEventListener("click", () => fetch(data.info[button.classList[0]])
+        .then(res => res.json())
+        .then(newData => makeDomFromData(newData,rootElement))
+))
+   rootElement.insertAdjacentHTML("beforeend",charactersComponent(data.results));
+//     rootElement.insertAdjacentHTML("beforeend",nextButtonComponent("next"));
+//     rootElement.insertAdjacentHTML("beforeend",nextButtonComponent("prev"));
+//     rootElement.insertAdjacentHTML("beforeend",charactersComponent(data.results));
+    
+//     const nextButtonElement = document.querySelector("button.next");
+//     if (data.info.next) {
+// nextButtonElement.addEventListener("click", () => {
+//     fetch(data.info.next)
+//         .then(res => res.json())
+//         .then(newData => makeDomFromData(newData, rootElement));
+// })
+
+// } else {
+//     nextButtonElement.disabled = true;
+// }
+
+// const prevButtonElement = document.querySelector("button.prev");
+// if (data.info.prev) {
+//     prevButtonElement.addEventListener("click", () => {
+//         fetch(data.info.prev)
+//             .then(res => res.json())
+//             .then(newData => makeDomFromData(newData, rootElement));
+//     });
+// } else {
+//     prevButtonElement.disabled = true;
+// }
 };
+
+
 
 fetch("https://rickandmortyapi.com/api/character")
 .then((response) => response.json())
 .then((data) =>{
     console.log(data);
-
-    // data.results.forEach((characterData) => {
-    //     document.querySelector('#root').insertAdjacentHTML("beforeend",characterCard(characterData));
-    //     console.log("dom manipulation")
-    // })
+    const rootElement = document.querySelector("#root")
 
     document.querySelector("#root").insertAdjacentHTML("beforeend", charactersComponent(data.results));
-    console.log("dom manipulation");
+    makeDomFromData(data, rootElement);
 
-    makeDomFromData(data, document.querySelector("#root"));
+    // const nextButtonElement = document.querySelector("button")
+    // nextButtonElement.addEventListener("click", () => {
+    //     fetch(data.info.next)
+    //         .then(res => res.json())
+    //         .then(newData => {
+    //             console.log(newData);
+    //             makeDomFromData(newData,rootElement);
+    //             const newNextButtonElement = document.querySelector("button")
+    //             newNextButtonElement.addEventListener("click",() => {
+    //                 console.log("fetch next page");
+    //                 fetch(newData.info.next)
+    //                     .then(res2 => res2.json())
+    //                     .then(evenNewerData => {
+    //                         console.log(evenNewerData);
+    //                         makeDomFromData(evenNewerData, rootElement);
+    //                         const evenNewerNextButton = document.querySelector("button");
+    //                     })
+    //             })
+    //         })
+    // })
 })
